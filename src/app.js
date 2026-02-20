4e18411c6b31f935535b3688c6a05fdd982ba726
Ywatch15 <sundram.22460@knit.ac.in>
2026-02-20 13:50:26 +0530

    Created the accounts api and it's route, controller, and model.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const authRoutes = require("./routes/auth.routes");
-
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth",authRoutes);
+
+/**
+ * - Routes required here
+ */
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+
+
+/**
+ * - User Routes
+ */
+app.use("/api/auth",authRouter);
+app.use("/api/account",accountRouter);
 
 
 module.exports = app;
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+const userModel = require("../models/user.model"); 
+const jwt = require("jsonwebtoken");
+
+
+
+
+async function authMiddleware(req,res,next){
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            success:false,
+            message:"Unauthorized. No token provided."
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded.userId);
+
+        req.user=user
+        return next();
+    }catch(err){
+        return res.status(401).json({
+            message:"Unauthorized. Invalid token."
+        })
+    }
+}
+
+
+module.exports={
+    authMiddleware
+}
